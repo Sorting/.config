c56be2cd335c919b4747b8d691b036320afc8d59
Jimmy Sorting <jimmy.sorting@volvocars.com>
2023-04-19 15:38:48 +0200

    setups

--- plugins/treesitter.lua
+++ plugins/treesitter.lua
@@ -1,7 +1,10 @@
 return {
   "nvim-treesitter/nvim-treesitter",
   opts = {
-    -- ensure_installed = { "lua" },
+    ensure_installed = { "lua", "typescript", "go",
+      "json", "yaml", "html", "css", "javascript", "bash", "python", "rust", "c", "cpp", "java", "php", "regex", "toml",
+      "tsx", "vue", "yaml", "zig" },
+    autopairs = { enable = true },
     autotag = { enable = true },
   },
 }
