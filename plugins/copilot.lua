caacefa49414b6c260a3498eb59f895ed9396e4d
Jimmy Sorting <jimmy.sorting@volvocars.com>
2023-04-19 15:47:20 +0200

    fix error

--- plugins/copilot.lua
+++ plugins/copilot.lua
@@ -1,6 +1,6 @@
 vim.cmd [[imap <silent><script><expr> <C-a> copilot#Accept("\<CR>")]]
 vim.g.copilot_no_tab_map = true
-vim.cmd [[highlight CopilotMatch cterm=borderHasRightLined ctermfg=red guifg=#ff0000 gui=bold]]
+vim.cmd [[highlight CopilotMatch cterm=bold ctermfg=red guifg=#ff0000 gui=bold]]
 
 return {
   "github/copilot.vim",
